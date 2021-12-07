d22409c9b7493310f362e21fae1235360d2b3eeb
ROEL2407 <roel24komen@gmail.com>
2021-12-07 11:57:04 +0100

    create react pp from scratch

--- src/components/Home.js
+++
@@ -1,8 +0,0 @@
-const Home = () => 
-<>
-    <h1>Welcome to my Starter Pokémon application</h1>
-    <p>Have a look around!</p>
-    <p>Github repo: <a href="https://github.com/ROEL2407/Frontend-Applications">https://github.com/ROEL2407/Frontend-Applications</a></p>
-</>;
-
-export default Home;
