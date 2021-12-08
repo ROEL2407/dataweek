e8095526f97445b6eb2ff1c27486814abd903984
ROEL2407 <roel24komen@gmail.com>
2021-12-08 14:47:52 +0100

    Update: new route visualization Amanda

--- dataweek/src/App.js
+++ dataweek/src/App.js
@@ -4,17 +4,20 @@ import React from 'react';
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 import BarRoelPage from "./components/BarRoelPage";
+import BarAmandaPage from './components/BarAmandaPage';
 function App() {
 
   return (
     <div className="App">
       <div className="nav">
         <Link to="/">Home</Link>
-        <Link to="/barroel">Visualization Roel</Link>
+        <Link to="/bar-roel">Visualization Roel</Link>
+        <Link to="/bar-amanda">Visualization Amanda</Link>
       </div>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/barroel" element={<BarRoelPage />}/>
+        <Route path="/bar-roel" element={<BarRoelPage />}/>
+        <Route path="/bar-amanda" element={<BarAmandaPage />}/>
       </Routes>
     </div>
   );
