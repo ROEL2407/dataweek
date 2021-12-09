5e43a68e4cb912d1ffa803fb597f37c0a07c2474
ROEL2407 <roel24komen@gmail.com>
2021-12-09 14:56:06 +0100

    Update: aquire mondkapjes data

--- dataweek/src/components/BarRoelPage.js
+++ dataweek/src/components/BarRoelPage.js
@@ -1,34 +1,44 @@
 import BarChartRoel from "../charts/BarChartRoel";
 import { stationData } from "../json/stations";
+import { mondkapData } from  "../json/mondkapjes";
 
 
 function BarRoelPage () {
     const routeRoel = [];
+    const mondkap = mondkapData;
+    let roeldata = [];
+    if (mondkap[0].treinroute === "treinRoel") {
+      roeldata = mondkap[0].mondkapjes;
+    }
     
     stationData.forEach(element => {
         switch (element.name_long) {
             case "Heiloo":
             routeRoel.push({
                 id: 0,
-                Naam: element.name_long + " - Castricum"
+                Naam: element.name_long + " - Castricum",
+                mondkapjes: roeldata.hei_cas
             })
             break;
             case "Castricum":
             routeRoel.push({
                 id: 1,
-                Naam: element.name_long + " - Zaandam"
+                Naam: element.name_long + " - Zaandam",
+                mondkapjes: roeldata.cas_zaa
             })
             break;
             case "Zaandam":
             routeRoel.push({
                 id: 2,
-                Naam: element.name_long + " - Ams Sloterdijk"
+                Naam: element.name_long + " - Ams Sloterdijk",
+                mondkapjes: roeldata.zaa_slo
             })
             break;
             case "Amsterdam Sloterdijk":
             routeRoel.push({
                 id: 3,
-                Naam: element.name_long + " - Ams Centraal"
+                Naam: element.name_long + " - Ams Centraal",
+                mondkapjes: roeldata.slo_ams
             })
             break;
         
@@ -39,7 +49,7 @@ function BarRoelPage () {
     routeRoel.sort( function(a, b) { 
         return a.id - b.id  ||  a.naam.localeCompare(b.naam);
       });
-    console.log(routeRoel);
+    console.log(routeRoel)
   return (
     <>
       <BarChartRoel data={routeRoel} />
