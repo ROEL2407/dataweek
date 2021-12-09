eab847e444e5fd319b6d15a686fec48c024653b5
ROEL2407 <roel24komen@gmail.com>
2021-12-09 15:44:41 +0100

    Create: new charts for stijn

---
+++ dataweek/src/charts/stijn/BarChartStijn.js
@@ -0,0 +1,93 @@
+import useD3 from "../../hooks/useD3";
+import React from "react";
+import * as d3 from "d3";
+
+function BarChartStijn({ data }) {
+
+  const ref = useD3(
+    (svg) => {
+      const height = 400;
+      const width = 600;
+      const margin = {top: 40, bottom: 10, left: 200, right: 20 };
+
+      const g = svg.append("g")
+      .attr("transform", `translate(${margin.left},${margin.top})`)
+      .attr("id", "goed_diagram");
+        
+      const xscale = d3.scaleLinear().domain(0, 1).range([0, width]);
+      const yscale = d3.scaleBand().rangeRound([0, height]).paddingInner(0.1);
+
+      // Axis setup
+      const xaxis = d3.axisTop().scale(xscale);
+      const g_xaxis = g.append("g").attr("className", "x axis");
+      const yaxis = d3.axisLeft().scale(yscale);
+      const g_yaxis = g.append("g").attr("className", "y axis");
+
+      //color scale items of wearing
+      let myColor = d3.scaleOrdinal().domain(data)
+      .range(["#e8ba31"])
+      ///////////////////////
+      update(data);
+      console.log(data);
+
+      function update(new_data) {
+          console.log(new_data);
+          //update the scales
+          xscale.domain([0, 20]);
+          yscale.domain(new_data.map((d) => d.Naam));
+          //render the axis
+          g_xaxis.transition().call(xaxis);
+          g_yaxis.transition().call(yaxis);
+
+          const rect = g.selectAll("rect").data(new_data, (d) => d.mondkapjes.waar.goed).join(
+                  // ENTER 
+                  // new elements
+                  (enter) => {
+                      const rect_enter = enter.append("rect").attr("x", 1);
+                      rect_enter.append("title");
+                      return rect_enter;
+                  },
+                  // UPDATE
+                  // update existing elements
+                  (update) => update,
+                  // EXIT
+                  // elements that aren't associated with data
+                  (exit) => exit.remove()
+              )
+
+          // ENTER + UPDATE
+          // both old and new elements
+          rect.transition()
+              .attr("height", yscale.bandwidth())
+              .attr("width", (d) => xscale(d.mondkapjes.waar.goed))
+              .attr("y", (d) => yscale(d.Naam))
+              .attr("className", (d) => d.mondkapjes.waar.goed)
+              .style('fill',  function(d){return myColor(d)})
+              
+      }
+    },
+    [data.length]
+  );
+
+  return (
+      <>
+      <h2>Het aantal mensen die de mondkap goed dragen</h2>
+    <svg
+      ref={ref}
+      style={{
+        height: 500,
+        width: "100%",
+        marginRight: "0px",
+        marginLeft: "0px"
+      }}
+      className="starterStats" 
+    >
+      <g className="plot-area" />
+      <g className="x-axis" />
+      <g className="y-axis" />
+    </svg>
+    </>
+  );
+}
+
+export default BarChartStijn;
--- dataweek/src/components/BarStijnPage.js
+++ dataweek/src/components/BarStijnPage.js
@@ -1,44 +1,58 @@
-import BarChartStijn from "../charts/BarChartStijn";
+import BarChartStijn from "../charts/stijn/BarChartStijn";
+import BarChart2Stijn from "../charts/stijn/BarChart2Stijn";
+import BarChart3Stijn from "../charts/stijn/BarChart3Stijn";
 import { stationData } from "../json/stations";
+import { mondkapData } from "../json/mondkapjes";
 
 
-function BarStijnPage () {
+function BarStijnPage() {
     const routeStijn = [];
+    const mondkap = mondkapData;
+    let stijndata = [];
+    if (mondkap[1].treinroute === "treinStijn") {
+      stijndata = mondkap[1].mondkapjes;
+    }
+    console.log(stijndata);
     
     stationData.forEach(element => {
         switch (element.name_long) {
             case "Almelo":
                 routeStijn.push({
                 id: 0,
-                Naam: element.name_long + " - Deventer"
+                Naam: element.name_long + " - Deventer",
+                mondkapjes: stijndata.alm_dev
             })
             break;
 
             case "Deventer":
                 routeStijn.push({
                 id: 1,
-                Naam: element.name_long + " - Apeldoorn"
+                Naam: element.name_long + " - Apeldoorn",
+                mondkapjes: stijndata.dev_apl
             })
             break;
 
             case "Apeldoorn":
                 routeStijn.push({
                 id: 2,
-                Naam: element.name_long + " - Amersfoort Centraal"
+                Naam: element.name_long + " - Amersfoort Centraal",
+                mondkapjes: stijndata.apl_amf
             })
             break;
 
             case "Amersfoort Centraal":
                 routeStijn.push({
                 id: 3,
-                Naam: element.name_long + " - Hilversum"
+                Naam: element.name_long + " - Hilversum",
+                mondkapjes: stijndata.amf_hil
             })
             break;
 
             case "Hilversum":
                 routeStijn.push({
-                id: 3,
-                Naam: element.name_long + " - Ams Centraal"
+                id: 4,
+                Naam: element.name_long + " - Ams Centraal",
+                mondkapjes: stijndata.hil_ams
             })
             break;
         
@@ -46,13 +60,15 @@ function BarStijnPage () {
                 break;
         }
     });
+    console.log(routeStijn);
     routeStijn.sort( function(a, b) { 
         return a.id - b.id  ||  a.naam.localeCompare(b.naam);
       });
-    console.log(routeStijn);
   return (
     <>
-      <BarChartStijn data={routeStijn} />
+    <BarChartStijn data={routeStijn} />
+      <BarChart2Stijn data={routeStijn} />
+      <BarChart3Stijn data={routeStijn} />
     </>
   );
 }
