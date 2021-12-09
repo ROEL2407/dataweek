2822ef0e938982a9db80df17e6417715afe1134b
ROEL2407 <roel24komen@gmail.com>
2021-12-09 13:24:02 +0100

    Create: mondkapjes data in json

---
+++ dataweek/src/json/mondkapjes.js
@@ -0,0 +1,205 @@
+export const mondkapData = [
+    {
+        "treinroute": "treinRoel",
+        "mondkapjes":{
+            "hei-cas": {
+                "waar": {
+                    "goed": 9,
+                    "neus": 1,
+                    "kin": 2
+                },
+                "kleur": {
+                    "zwart": 6,
+                    "blauw": 5,
+                    "wit": 0,
+                    "anders": 0
+                },
+                "soort": {
+                    "stof": 4,
+                    "wegwerp": 7
+                }
+            },
+            "cas-zaa": {
+                "waar": {
+                    "goed": 9,
+                    "neus": 1,
+                    "kin": 2
+                },
+                "kleur": {
+                    "zwart": 6,
+                    "blauw": 5,
+                    "wit": 0,
+                    "anders": 0
+                },
+                "soort": {
+                    "stof": 5,
+                    "wegwerp": 6
+                }
+            },
+            "zaa-slo": {
+                "waar": {
+                    "goed": 9,
+                    "neus": 1,
+                    "kin": 3,
+                },
+                "kleur": {
+                    "zwart": 6,
+                    "blauw": 7,
+                    "wit": 0,
+                    "anders": 0
+                },
+                "soort": {
+                    "stof": 5,
+                    "wegwerp": 8
+                }
+            },
+            "slo-ams": {
+                "waar": {
+                    "goed": 11,
+                    "neus": 1,
+                    "kin": 0,
+                },
+                "kleur": {
+                    "zwart": 6,
+                    "blauw": 6,
+                    "wit": 0,
+                    "anders": 0
+                },
+                "soort": {
+                    "stof": 7,
+                    "wegwerp": 5
+                }
+            }
+        }
+    },
+    {
+        "treinroute": "treinStijn",
+        "mondkapjes":{
+            "alm-dev": {
+                "waar": {
+                    "goed": 7,
+                    "neus": 1,
+                    "kin": 3,
+                },
+                "kleur": {
+                    "zwart": 4,
+                    "blauw": 2,
+                    "wit": 1,
+                    "anders": 1
+                },
+                "soort": {
+                    "stof": 1,
+                    "wegwerp": 7
+                }
+            },
+            "dev-apl": {
+                "waar": {
+                    "goed": 19,
+                    "neus": 4,
+                    "kin": 5,
+                },
+                "kleur": {
+                    "zwart": 15,
+                    "blauw": 3,
+                    "wit": 3,
+                    "anders": 2
+                },
+                "soort": {
+                    "stof": 4,
+                    "wegwerp": 20
+                }
+            },
+            "apl-amf": {
+                "waar": {
+                    "goed": 16,
+                    "neus": 4,
+                    "kin": 3,
+                },
+                "kleur": {
+                    "zwart": 12,
+                    "blauw": 4,
+                    "wit": 2,
+                    "anders": 1
+                },
+                "soort": {
+                    "stof": 4,
+                    "wegwerp": 15
+                }
+            },
+            "amf-hil": {
+                "waar": {
+                    "goed": 10,
+                    "neus": 1,
+                    "kin": 3,
+                },
+                "kleur": {
+                    "zwart": 5,
+                    "blauw": 4,
+                    "wit": 0,
+                    "anders": 2
+                },
+                "soort": {
+                    "stof": 3,
+                    "wegwerp": 8
+                }
+            },
+            "hil-ams": {
+                "waar": {
+                    "goed": 15,
+                    "neus": 3,
+                    "kin": 4,
+                },
+                "kleur": {
+                    "zwart": 6,
+                    "blauw": 5,
+                    "wit": 3,
+                    "anders": 3
+                },
+                "soort": {
+                    "stof": 3,
+                    "wegwerp": 15
+                }
+            }
+        }
+    },
+    {
+        "treinroute": "treinAmanda",
+        "mondkapjes":{
+            "hoo-sch": {
+                "waar": {
+                    "goed": 3,
+                    "neus": 2,
+                    "kin": 2
+                },
+                "kleur": {
+                    "zwart": 2,
+                    "blauw": 4,
+                    "wit": 0,
+                    "anders": 1
+                },
+                "soort": {
+                    "stof": 2,
+                    "wegwerp": 5
+                }
+            },
+            "sch-zui": {
+                "waar": {
+                    "goed": 4,
+                    "neus": 1,
+                    "kin": 1,
+                },
+                "kleur": {
+                    "zwart": 2,
+                    "blauw": 2,
+                    "wit": 2,
+                    "anders": 0
+                },
+                "soort": {
+                    "stof": 1,
+                    "wegwerp": 5
+                }
+            }
+            
+        }
+    }
+]
